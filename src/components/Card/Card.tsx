83a7ed84b6f070d2fdfc024baca1994d8d7ce86d
Vova Pol <ya-chai777@yandex.ru>
2023-06-07 19:43:22 +0300

    базовая верстка компонента Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,29 @@
 import { FC } from 'react';
+import './Card.css';
 import { Book } from '../../store/types';
 
+import {
+  AiOutlineShoppingCart,
+  AiFillHeart,
+  AiOutlineHeart,
+} from 'react-icons/ai';
+
 export const Card: FC<Book> = ({ image, price, title, subtitle }) => {
-  return <li className="card">{title}</li>;
+  return (
+    <li className="card">
+      <img className="card__image" src={image}></img>
+      <div className="card__info">
+        <h3 className="card__title">{title}</h3>
+        <p className="card__price">{price}</p>
+      </div>
+      <div className="card__buttons">
+        <button type="button" className="card__cart-button">
+          <AiOutlineShoppingCart />
+        </button>
+        <button type="button" className="card__favourite-button">
+          <AiOutlineHeart />
+        </button>
+      </div>
+    </li>
+  );
 };
