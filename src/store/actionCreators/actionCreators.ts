f257f4cb34c3b66c0ab02d9f290b092729c0cb94
Vova Pol <ya-chai777@yandex.ru>
2023-11-19 10:25:12 +0300

    Убраны лишние импорты и комментарии из actionCreators

--- src/store/actionCreators/actionCreators.ts
+++ src/store/actionCreators/actionCreators.ts
@@ -5,12 +5,7 @@ import {
   IBook,
 } from '../types';
 import { sendGetRequest } from '../../utils/utils';
-import {
-  BOOKS_LS_KEY,
-  BOOKS_URL,
-  CART_LS_KEY,
-  FAVOURITES_LS_KEY,
-} from '../../utils/constants';
+import { BOOKS_URL } from '../../utils/constants';
 import { Dispatch } from 'redux';
 
 export const getBooksList = () => {
@@ -39,20 +34,6 @@ export const addToFavourites = (book: IBook) => {
       type: ACTION_TYPES_FAVOURITES.ADD_TO_FAVOURITES,
       payload: book,
     });
-
-    try {
-      if (localStorage.getItem(FAVOURITES_LS_KEY)) {
-        const favourites = JSON.parse(localStorage.getItem(FAVOURITES_LS_KEY)!);
-        favourites.push(book);
-        localStorage.setItem(FAVOURITES_LS_KEY, JSON.stringify(favourites));
-      } else {
-        localStorage.setItem(FAVOURITES_LS_KEY, JSON.stringify([book]));
-      }
-    } catch (err: any) {
-      console.error('Ошибка при записи данных в локальное хранилище.');
-      console.error(`Запись объекта ${book} с ключом ${FAVOURITES_LS_KEY}`);
-      console.error({ errorMessage: err.message });
-    }
   };
 };
 
@@ -62,20 +43,6 @@ export const removeFromFavourites = (book: IBook) => {
       type: ACTION_TYPES_FAVOURITES.REMOVE_FROM_FAVOURITES,
       payload: book,
     });
-
-    try {
-      if (localStorage.getItem(FAVOURITES_LS_KEY)) {
-        const favourites = JSON.parse(localStorage.getItem(FAVOURITES_LS_KEY)!);
-        const newFavourites = favourites.filter(
-          (fav: IBook) => fav.isbn13 !== book.isbn13,
-        );
-        localStorage.setItem(FAVOURITES_LS_KEY, JSON.stringify(newFavourites));
-      }
-    } catch (err: any) {
-      console.error('Ошибка при записи данных в локальное хранилище.');
-      console.error(`Удаление объекта ${book} с ключом ${FAVOURITES_LS_KEY}`);
-      console.error({ errorMessage: err.message });
-    }
   };
 };
 
@@ -85,62 +52,11 @@ export const addToCart = (book: IBook) => {
       type: ACTION_TYPES_CART.ADD_TO_CART,
       payload: book,
     });
-
-    // try {
-    //   if (!localStorage.getItem(CART_LS_KEY)) {
-    //     localStorage.setItem(CART_LS_KEY, JSON.stringify([[book, 1]]));
-    //   } else {
-    //     const cart = JSON.parse(localStorage.getItem(CART_LS_KEY)!);
-    //     const bookIsInCart = cart.some(
-    //       (entry: [IBook, number]) => entry[0].isbn13 === book.isbn13,
-    //     );
-    //     if (bookIsInCart) {
-    //       const newCart = cart.map((entry: [IBook, number]) => {
-    //         return entry[0].isbn13 === book.isbn13
-    //           ? [book, entry[1] + 1]
-    //           : entry;
-    //       });
-
-    //       localStorage.setItem(CART_LS_KEY, JSON.stringify(newCart));
-    //     } else {
-    //       cart.push([book, 1]);
-    //       localStorage.setItem(CART_LS_KEY, JSON.stringify(cart));
-    //     }
-    //   }
-    // } catch (err: any) {
-    //   console.error('Ошибка при записи данных в локальное хранилище.');
-    //   console.error(`Запись объекта ${book} с ключом ${CART_LS_KEY}`);
-    //   console.error({ errorMessage: err.message });
-    // }
   };
 };
 
 export const removeFromCart = (book: IBook) => {
   return (dispatch: Dispatch) => {
     dispatch({ type: ACTION_TYPES_CART.REMOVE_FROM_CART, payload: book });
-
-    // try {
-    //   if (!localStorage.getItem(CART_LS_KEY)) {
-    //     throw new Error('Попытка удалить несуществующий объект');
-    //   } else {
-    //     const cart = JSON.parse(localStorage.getItem(CART_LS_KEY)!);
-    //     const newCart = cart.map((entry: [IBook, number]) => {
-    //       if (entry[0].isbn13 === book.isbn13) {
-    //         return [book, entry[1] - 1];
-    //       } else {
-    //         return entry;
-    //       }
-    //     });
-    //     const filteredCart = newCart.filter(
-    //       (entry: [IBook, number]) => entry[1] !== 0,
-    //     );
-
-    //     localStorage.setItem(CART_LS_KEY, JSON.stringify(filteredCart));
-    //   }
-    // } catch (err: any) {
-    //   console.error('Ошибка при записи данных в локальное хранилище.');
-    //   console.error(`Удаление объекта ${book} с ключом ${CART_LS_KEY}`);
-    //   console.error({ errorMessage: err.message });
-    // }
   };
 };
